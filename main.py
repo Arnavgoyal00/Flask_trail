be240821e9f69b909a2b39ec027d252ee84c989f
Arnav Goyal <garnav2002@gmail.com>
2022-06-25 14:41:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,7 @@ app = Flask(__name__)
 
 @app.route("/", methods =['GET','POST'])
 def home():
-    if request.method =='POST':
-        model = pickle.load(open('model.pkl','rb'))
-        user_input=request.form.get('size')
-        print(user_input)
-        user_input=float(user_input)
-        pred = model.predict([[user_input]])
-        print(pred)
-    return render_template('index.html',value=pred)
+    return render_template('index.html')
 
 if __name__ == "_main_":
     app.run(debug=True)

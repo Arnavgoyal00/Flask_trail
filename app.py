67b36a001f6e54e0c4daa8bbbaf7ac89094c28ef
Arnav Goyal <garnav2002@gmail.com>
2022-06-25 17:02:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/", methods =['GET','POST'])
 def home():
-    return "Hello, World!kfkyh"
+    return "Arnav Here"
 
 if __name__ == "_main_":
     app.run(debug=True)
